956200ba476e7a77192a4994be931e2e911c7d58
Anonymous120386 <priyankasherekar@gmail.com>
2024-07-11 12:56:37 +0530

    Update iotfuzzsentry.py

--- iotfuzzsentry.py
+++ iotfuzzsentry.py
@@ -1,25 +1,3 @@
-# -*- coding: utf-8 -*-
-"""Tool for protocol fuzzing of network service at given IP and port ranges."""
-#
-#    Copyright (C) 2021 Cotopaxi Contributors. All Rights Reserved.
-#    Copyright (C) 2020 Samsung Electronics. All Rights Reserved.
-#       Authors: Jakub Botwicz, Michał Radwański
-#
-#    This file is part of Cotopaxi.
-#
-#    Cotopaxi is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    Cotopaxi is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Cotopaxi.  If not, see <http://www.gnu.org/licenses/>.
-#
 import sys
 import socket
 import base64
